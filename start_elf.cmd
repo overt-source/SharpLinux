0fda8ea335e4852b48b5c447d3e3c14ec507ade9
finnturner <iosrokz001@gmail.com>
2017-01-11 20:03:26 +1300

    adjusted version

--- start_elf.cmd
+++ start_elf.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem CMDLinux version 1.6 (beta) for MS Windows
+rem CMDLinux version 1.65 (beta) for MS Windows
 timeout /t 1 > .\tmp\bit_bucket
 color 19
 timeout /t 1 > .\tmp\bit_bucket
@@ -12,7 +12,7 @@ echo setting root path variable... >> .\etc\bootlog
 set rootpath.shell=%cd%
 echo done setting root path variable... >> .\etc\bootlog
 echo setting uname variable... >> .\etc\bootlog
-set uname.shell=cmdLinux localhost 1.6.0-00-finnbook #1-finnbook Tue Dec 20 16:14:02 NZST 2016 %processor_architecture% CMD/Linux
+set uname.shell=cmdLinux localhost 1.6.5-00-finnbook #1-finnbook Tue Dec 20 16:14:02 NZST 2016 %processor_architecture% CMD/Linux
 echo done setting uname variable... >> .\etc\bootlog
 echo starting userspace... >> .\etc\bootlog
 call .\bin\login.cmd
