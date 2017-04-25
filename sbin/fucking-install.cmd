08320667bf1d2750e56ac66b18d01b8efdb639af
finn turner <finnf@mariah.(none)>
2017-04-25 15:07:36 +1200

    started work on fucking propper and implemented hiding of dotfiles in ls listings (~/.*)

--- sbin/fucking-install.cmd
+++ sbin/fucking-install.cmd
@@ -1,8 +1,25 @@
 @echo off
-rem calls fucking(1) to install package
-set /p package.install.shell=Fucking: enter package name to install: 
-if %package.install.shell% == curl (
-echo curl is already here, otherwise fucking wouldnt've started
-echo curl: preinst
+echo fucking - a high-level package manager for the CMDLinux Operating system
+rem check for curl/wget
+rem we need this across all binaries
+if exist "%rootpath.shell%\lib\curl.exe" goto main
+if exist "%rootpath.shell%\lib\wget.exe" goto main
+if not exist "%rootpath.shell%\lib\curl.exe" goto liberror
+if not exist "%rootpath.shell%\lib\wget.exe" goto liberror
+:liberror
+echo fucking: E: 1, in init.install, failed to locate network access libraries in /lib
 goto:eof
+:main
+set /p fuckingsource.shell=< %rootpath.shell%\etc\fucking\source
+echo fucking: using source %fuckingsource.shell%
+set /p fucking.package.name=fucking: need package identifyer: 
+if exist %rootpath.shell%\etc\fucking\packages\%fucking.package.name% (
+goto install
 )
+if not exist %rootpath.shell%\etc\fucking\packages\%fucking.package.name% (
+echo fucking: fatal: %fucking.package.name%: NOT A PACKAGE IDENTIFYER, OR NOT IN THE LOCAL DATABASE.
+echo fucking: If you're sure it's there, do fucking-update to make sure we have the latest
+echo fucking: packages.
+echo fucking exited with error code: 2
+)
+goto:eof
--- sbin/fucking.cmd
+++ sbin/fucking.cmd
@@ -9,6 +9,9 @@ if not exist "%rootpath.shell%\lib\wget.exe" goto liberror
 echo fucking: E: 1, in init, failed to locate network access libraries in /lib
 goto:eof
 :main
+set /p fuckingsource.shell=< %rootpath.shell%\etc\fucking\source
+echo fucking: using source %fuckingsource.shell%
 echo fucking: need an operation (like -install,-upgrade,-upgrade-all)
+echo fucking: check doc fucking for a list of appropriate commands
 
 goto:eof
