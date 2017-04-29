d0ce1f8563bf96c75aa7ef0e1e6f30d9bb9233e3
finn turner <Admin@Gallifrey.xer>
2017-04-29 20:48:00 +1200

    added a codename to the cmdlinux uname.

--- init.cmd
+++ init.cmd
@@ -4,14 +4,15 @@ rem THIS SCRIPT SHOULD NOT BE MODIFIED BY
 rem PEOPLE WHO DON'T KNOW WHAT THEY'RE DOING!
 title CMDLinux starting...
 set version.shell=2.2.1
-set CompileTime.shell=2017-04-28 19:52
+set CompileTime.shell=2017-04-29t20:46:31+12:00
+set codename.shell=mariah
 set buildid.shell=221
-set build.shell=0005
+set build.shell=0006
 echo STARTING CMDLinux (%processor_architecture%):%processor_revision%
 echo on-line CPUs: %number_of_processors%
 set rootpath.shell=%cd%
 timeout /t 6 >> %rootpath.shell%\etc\bootlog
 echo Welcome to CMDLinux!
-echo CMDLinux %computername% %version.shell%(%buildid.shell%.%build.shell%/%processor_architecture%-windows-master) %CompileTime.shell%, %processor_architecture% Cmdlinux
-set uname.shell=CMDLinux %computername% %version.shell%(%buildid.shell%.%build.shell%/%processor_architecture%-windows-master) %CompileTime.shell%, %processor_architecture% Cmdlinux
+echo CMDLinux %version.shell% %codename.shell%:(%buildid.shell%.%build.shell%/%processor_architecture%-windows-master) %CompileTime.shell%, %processor_architecture% Cmdlinux
+set uname.shell=CMDLinux %version.shell% %codename.shell%:(%buildid.shell%.%build.shell%/%processor_architecture%-windows-master) %CompileTime.shell%, %processor_architecture% Cmdlinux
 call %rootpath.shell%\bin\login
