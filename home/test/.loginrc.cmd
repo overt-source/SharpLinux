de77f2d2a5266c44dbd505add0f4abf998bece2d
Finn Turner <finn@mail.inner-visions.org>
2017-05-01 14:57:23 +1200

    modified all .loginrc to publish copyright and version information on login.

--- home/test/.loginrc.cmd
+++ home/test/.loginrc.cmd
@@ -6,4 +6,9 @@ set shell=sh
 set shebang=$
 cd %rootpath.shell%\home\%username.shell%\
 set command.shell=undefined
+echo (_)(_)(_)(_)(_)(_)(_)(_)(_)(_)
+echo CMDLINUX version %version.shell% %codename.shell% compiled on %compiletime.shell%
+echo (_)(_)(_)(_)(_)(_)(_)(_)(_)(_)
+echo THIS SOFTWARE IS published UNDER THE GNU GENERAL PUBLIC LICENSE VERSION 2 AND IS COPYRIGHT (C) 2017 FINN TURNER AND CONTRIBUTERS. SEE /LICENCE FOR MORE INFORMATION.
+
 call "%rootpath.shell%\bin\%shell%"
