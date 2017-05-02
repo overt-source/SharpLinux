928abfd8c8d349d6fba030709e07d13fc50a43f0
Finn Turner <finn@mail.inner-visions.org>
2017-05-02 12:48:42 +1200

    added basic fucking update script (only half done).

---
+++ sbin/fucking-update.cmd
@@ -0,0 +1,11 @@
+@echo off
+echo fucking - a high-level package manager for the CMDLinux Operating system
+:main
+set /p fuckingsource.shell=< %rootpath.shell%\etc\fucking\source
+echo fucking: using source %fuckingsource.shell%
+echo Fucking: Fetching package indexes...
+%rootpath.shell%\lib\libwebget.module %fuckingsource.shell%/packages.tar %rootpath.shell%\etc\fucking\packages.tar
+if %errorlevel% == 5 (
+echo FUCKING: FATAL: INTERNET CONNECTION OR SERVER ERROR WHILE FETCHING PACKAGES.TAR FROM %fuckingsource.shell%
+)
+goto:eof
