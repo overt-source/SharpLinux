16c00ac5498e27b278b9c155ac129ddb0e6f50b7
Finn Turner <finn@mail.inner-visions.org>
2017-05-08 12:17:07 +1200

    archived binaries

--- sbin/fucking.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo fucking - a high-level package manager for the CMDLinux Operating system
-:main
-set /p fuckingsource.shell=< %rootpath.shell%\etc\fucking\source
-echo fucking: using source %fuckingsource.shell%
-echo fucking: need an operation (like -install,-upgrade,-upgrade-all)
-echo fucking: check doc fucking for a list of appropriate commands
-
-goto:eof
