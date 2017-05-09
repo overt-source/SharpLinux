ad7c00c785ca97d817182317fef5e34085ea724d
Finn Turner <finn@mail.inner-visions.org>
2017-05-09 12:12:41 +1200

    checking something.

--- bin/login.cs
+++ bin/login.cs
@@ -9,9 +9,12 @@ System.Threading.Thread.Sleep(2000);
 Console.Clear();
 // set up some strings based on variables that init helpfully setup for us:
 string LoginHostSource=Environment.GetEnvironmentVariable("hostname.sl");
+// strip crlf from /etc/hostname
 string LoginHost = System.Text.RegularExpressions.Regex.Replace(LoginHostSource, @"\t|\n|\r", "");
 Console.Write(""+LoginHost+" Login:");
-Console.ReadLine();
+string username=Console.ReadLine();
+Console.Write("Password:");
+string password=Console.ReadLine();
 }
 }
 }
