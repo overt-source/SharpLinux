9e827a683f6e57a4fc82c5206029f9c4fec92dc7
Finn Turner <finn@mail.inner-visions.org>
2017-07-26 08:28:19 +1200

    added multi-column LS and rm

--- usr/share/src/bin/ls/ls.cs
+++ usr/share/src/bin/ls/ls.cs
@@ -10,13 +10,27 @@ Console.WriteLine("Total "+total+"");
 
 for (int i = 0; i < FolderPaths.Length; ++i) {
 
-    string path = FolderPaths[i];
-    Console.WriteLine(System.IO.Path.GetFileName(path));
+string path = FolderPaths[i];
+Console.Write("D ");
+Console.Write("WIN:");
+Console.Write(System.IO.File.GetAccessControl(path).GetOwner(typeof(System.Security.Principal.NTAccount)).ToString());
+Console.Write(" ");
+Console.Write(path.Length);
+Console.Write(" ");
+Console.Write(System.IO.Path.GetFileName(path));
+Console.Write("\r\n");
+
 }
 
 for (int i = 0; i < filePaths.Length; ++i) {
     string path = filePaths[i];
-    Console.WriteLine(System.IO.Path.GetFileName(path));
+Console.Write(". ");
+Console.Write("WIN:");
+Console.Write(System.IO.File.GetAccessControl(path).GetOwner(typeof(System.Security.Principal.NTAccount)).ToString());
+Console.Write(" ");
+Console.Write(path.Length);
+Console.Write(System.IO.Path.GetFileName(path));
+Console.Write("\r\n");
 }
 
 }
