be36d88843967b9cb87978ddb4c57fd8ec7abeca
Finn Turner <finn@mail.inner-visions.org>
2017-08-01 14:57:10 +1200

    added the VIM editor!

--- usr/share/src/bin/vi/vi.cs
+++ usr/share/src/bin/vi/vi.cs
@@ -20,15 +20,39 @@ Console.WriteLine("Decompressing the editor");
 var Loader = new Process();
 Loader.StartInfo = new ProcessStartInfo( ""+RootPath+"\\libexec\\BusyBox.em" ) 
 {
-        Arguments = "tar -xf "+RootPath+"\\libexec\\LibEdit.cem",
+        Arguments = "tar -xf "+RootPath+"\\libexec\\LibEdit.cem -C "+RootPath+"\\tmp\\",
         UseShellExecute = false,
 RedirectStandardOutput = true,
         CreateNoWindow = true
 };
 Loader.Start();
 Loader.WaitForExit ();
-
+Console.WriteLine("Initialising the WindowServer.");
 Console.WriteLine("Loading  "+file+"...");
+
+var editor = new Process();
+editor.StartInfo = new ProcessStartInfo( ""+RootPath+"\\tmp\\LibEdit\\vim.exe" ) 
+{
+        Arguments = file,
+        UseShellExecute = true,
+        CreateNoWindow = true
+};
+editor.Start();
+Console.WriteLine("\aWindowServer started.");
+Console.WriteLine("To interrupt, exit application from other window.");
+editor.WaitForExit ();
+Console.WriteLine("Shutting down the editor.");
+var remover = new Process();
+remover.StartInfo = new ProcessStartInfo( ""+RootPath+"\\libexec\\BusyBox.em" ) 
+{
+        Arguments = "rm -rf "+RootPath+"\\tmp\\LibEdit",
+        UseShellExecute = false,
+RedirectStandardOutput = true,
+        CreateNoWindow = true
+};
+remover.Start();
+remover.WaitForExit ();
+
 }
 }
 }
